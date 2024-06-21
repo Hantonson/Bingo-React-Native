8a635409de065d733059c51211fa429437e6de8d
Antonio Gabarrús Nerin <gabatono@gmail.com>
2024-06-21 09:50:46 +0100

    Modal Implementation

---
+++ styles/BingoModalStyles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const BingoModalStyles = StyleSheet.create({
+    modalContainer:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:'#00000080'
+    },
+    modalBody:{
+        backgroundColor:'#b0b0b0',
+        justifyContent:"center",
+        alignItems:"center",
+        borderRadius:30
+    }
+});
+
+export { BingoModalStyles };
